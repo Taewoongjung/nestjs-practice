4bca5f96a318123addfa1b0acdadd3eb64ac4434
Taewoongjung <aipooh8882@naver.com>
2022-01-07 16:38:17 +0900

    posting function code

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from "./boards.service";
+import { Board } from "./board.model";
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService){}
 
   @Get('/')
-  getAllBoard() { // 모든 게시물을 가져오는 핸들러(handdler)
+  getAllBoard(): Board[] { // 모든 게시물을 가져오는 핸들러(handdler)
     return this.boardsService.getAllBoards();
   }
 }
-
-
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import { Board, BoardStatus } from "./board.model";
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
-  private boards = [1,2]; // 게시판 데이터를 담기 위함
+  private boards: Board[] = []; // 게시판 데이터를 담기 위함
                       // 다른 컴포넌트에서 값을 수정하는 것을 막기 위해 private 사용함.
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards; // 배열에 들어있는 모든 값을 호출
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(), // uuid는 unique한 id를 주기 위함
+      title: title, // title
+      description: description, // description
+      status: BoardStatus.PUBLIC
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
