1db3fb6675891430076ebb59a4cba17af02134e9
Taewoongjung <aipooh8882@naver.com>
2022-01-17 23:27:41 +0900

    service for createBoard

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Board } from "./board.entity";
 import { BoardRepository } from "./board.repository";
+import { CreateBoardDto } from "./dto/create-board.dto";
+import { BoardStatus } from "./board-status.enum";
 
 @Injectable()
 export class BoardsService {
@@ -10,17 +12,22 @@ export class BoardsService {
     private boardRepository: BoardRepository,
   ) {}
 
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const {title, description} = createBoardDto;
+    const board = this.boardRepository.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC
+    })
+    await this.boardRepository.save(board);
+    return board;
+  }
+
   async getBoardById(id: number): Promise <Board> {
     const found = await this.boardRepository.findOne(id);
-
     if(!found) {
       throw new NotFoundException(`Can't find Board with id ${id}`);
     }
-
     return found;
-  }
-
-  createBoard(createBoardDto): Promise <Board> {
-    return this.boardRepository.createBoard(createBoardDto);
   }
 }
