a487cc6f4899cec3da29731e2acf01f27a6e227f
Taewoongjung <aipooh8882@naver.com>
2022-02-18 15:00:40 +0900

    basic signup, signin completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "./user.repository";
 import { Injectable } from "@nestjs/common";
+import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository
   ) { }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
 }
