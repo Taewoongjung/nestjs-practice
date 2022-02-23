c2c9b7dd329ea2f9fbb4be71b2a7986c84160016
Taewoongjung <aipooh8882@naver.com>
2022-02-23 14:15:49 +0900

    Encryption system, login system added

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,13 +1,28 @@
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username: username, password: password});
 
-    await this.save(user);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username: username, password: hashedPassword});
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      // 유저이름에 유니크한 값 주기
+      if(error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
